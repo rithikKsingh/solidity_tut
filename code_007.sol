3c10ed5f2a2af87de9a673d4e119fa6db360dc3c
Rithik Kumar Singh <112690734+rithikKsingh@users.noreply.github.com>
2024-12-13 00:18:32 +0530

    Update code_007.sol

--- code_007.sol
+++ code_007.sol
@@ -20,3 +20,14 @@ contract FundMe{
 // remains unchanged. 
 //Note: for failed transaction, gas is used and it cannot be recovered. as miners need to be compensated.
 //However you can specify the amound of gas to be used. 
+
+//When a revert is triggered in a smart contract, the following occurs:
+//Undo All Operations: Any changes made to the blockchain during the execution of the transaction 
+//(like updating balances or modifying storage) are rolled back to their original state, as though the transaction never happened.
+//Return Remaining Gas: The portion of gas that was allocated for the transaction but not used up is returned to the sender 
+//(the account initiating the transaction). However, any gas already consumed for the operations leading up to the revert is not refunded because the work was still performed by the network's nodes.
+//For example:
+
+//If a transaction is allotted 50,000 gas, but only 30,000 gas is consumed before a revert, the remaining 20,000 gas is returned to the sender.
+//However, the sender still loses the cost of the 30,000 gas consumed.
+//This ensures fairness because computational resources used up to the point of failure are compensated, but the system prevents further wastage of resources by halting execution.
