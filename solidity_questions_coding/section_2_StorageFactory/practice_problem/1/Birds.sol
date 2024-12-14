3045318acca123a16b76242fc5782ed413bfc84e
Rithik Kumar Singh <112690734+rithikKsingh@users.noreply.github.com>
2024-12-14 19:18:23 +0530

    Update Birds.sol

--- solidity_questions_coding/section_2_StorageFactory/practice_problem/1/Birds.sol
+++ solidity_questions_coding/section_2_StorageFactory/practice_problem/1/Birds.sol
@@ -1,1 +1,20 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract Birds{
+    // event Log(string);
+
+    // constructor(){
+    //     emit Log("Birds contract deployed");
+    // }
+
+    // function birdsChirp() public {
+    //     emit Log("Birds chirp");
+    // }
+
+    string public sound;
+    constructor(string memory _sound){
+        sound=_sound;
+    }        
+}
